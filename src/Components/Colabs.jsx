70e9ecdb69eb6941fa83dd84854382e087d18077
Gabriel Perez <gabo.leonp@gmail.com>
2023-04-18 20:16:48 -0400

    Termino

---
+++ src/Components/Colabs.jsx
@@ -0,0 +1,17 @@
+export const Colabs = [
+    {
+    id: "1",
+    nombre: "Drake",
+    correo: "Drake@Massive.com"
+    },
+    {
+    id: "2",
+    nombre: "The Weeknd",
+    correo: "Theweeknd@abeltes.com"
+    },
+    {
+    id: "3",
+    nombre: "Lil Wayne",
+    correo: "wayne@lilwayne.com"
+    },
+]
